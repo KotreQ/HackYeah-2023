ca88230986245c6549030209fa03c994f5bb9c73
Navio1430 <73886336+Navio1430@users.noreply.github.com>
2023-09-30 11:27:24 +0200

    Initialized client structure

---
+++ client/src/main.ts
@@ -0,0 +1,17 @@
+import './assets/main.css';
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import VueAxios from "vue-axios";
+import axios from "axios";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue);
+app.use(VueAxios, axios);
+
+app.provide('axios', app.config.globalProperties.axios);
+
+app.mount('#app');
