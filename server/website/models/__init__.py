d267387e984a51c09eb9d42c78cffa9253b40ecc
Radosław Wolanski <radekw8733@gmail.com>
2023-10-01 03:33:20 +0200

    School and field of study relationship structure

--- server/website/models/__init__.py
+++ server/website/models/__init__.py
@@ -2,8 +2,20 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+school_and_field = db.Table(
+    "school_field",
+    db.Column("school_id", db.Integer, db.ForeignKey("school.id")),
+    db.Column("field_of_study_id", db.Integer, db.ForeignKey("field_of_study.id"))
+)
+
 class School(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, unique=True)
-    school_type = db.Column(db.String)
-    coordinates = db.Column(db.String)
+    name = db.Column(db.String(160), unique=True)
+    school_type = db.Column(db.String(40))
+    coordinates = db.Column(db.String(60))
+
+class FieldOfStudy(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    description = db.Column(db.String(300))
+    schools = db.relationship("School", secondary=school_and_field, backref="school")
