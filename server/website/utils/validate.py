c1ad106ef619bb227d909aa243279b39662236cb
Bartłomiej Kochanek <bkochanek4@gmail.com>
2023-10-01 07:40:16 +0200

    Create object validator class

--- server/website/utils/validate.py
+++ server/website/utils/validate.py
@@ -6,6 +6,27 @@ class Validator:
         return all([condition(value) for condition in self.conditions])
 
 
+class ObjectValidator:
+    def __init__(self, validators_dict):
+        for attr_name, attr_validator in validators_dict.items():
+            if not isinstance(attr_name, str):
+                raise TypeError(
+                    f"attr_name must be a string, instead got: {attr_name!r}"
+                )
+            if not isinstance(attr_validator, Validator):
+                raise TypeError(
+                    f"attr_validator must be a Validator, instead got: {attr_validator!r}"
+                )
+        self.validators_dict = validators_dict
+
+    def __call__(self, obj_value):
+        for attr_name, attr_validator in self.validators_dict.items():
+            attr_value = getattr(obj_value, attr_name, None)
+            if not attr_validator(attr_value):
+                return False
+        return True
+
+
 def eq_type(type_):
     return lambda x: isinstance(x, type_)
 
