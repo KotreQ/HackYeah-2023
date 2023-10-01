94f54b8b8627dfd4bdf76a4923f2dd5d8b0dc88f
Radosław Wolanski <radekw8733@gmail.com>
2023-10-01 02:22:58 +0200

    Basic school database structure

--- server/website/models/__init__.py
+++ server/website/models/__init__.py
@@ -1,3 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class School(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+    school_type = db.Column(db.String)
+    coordinates = db.Column(db.String)
